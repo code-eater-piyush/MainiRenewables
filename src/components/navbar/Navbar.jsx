932013e2e0b7aeaf92e1461bb304b03e2d9c2b51
code-eater-piyush <kpiyush032005@gmail.com>
2025-03-22 12:40:04 +0530

    contact page

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -2,7 +2,6 @@ import React, { useState, useEffect, useRef } from 'react';
 import { Link } from 'react-router-dom';
 import { IoReorderThreeOutline, IoClose } from 'react-icons/io5';
 import Sidebar from '../sidebar/Sidebar';
-import logo from '../../assets/Maini-Renewables_Final.webp';
 import './navbar.css';
 
 const Navbar = () => {
@@ -22,11 +21,6 @@ const Navbar = () => {
     <div className="navbar-container">
       <Sidebar isOpen={isSidebarOpen} onClose={toggleMenu} />
       <div className="navbar" ref={navbarRef}>
-        <div className="navbar-logo">
-          <Link to="/">
-            <img src={logo} alt="Logo" />
-          </Link>
-        </div>
         <div className="menu-list">
           <ul className="navbar-menu">
             <li><Link to="/">Home</Link></li>
@@ -35,21 +29,13 @@ const Navbar = () => {
             <li><Link to="/about">About</Link></li>
             <li><Link to="/contact">Contact Us</Link></li>
           </ul>
-          <ul>
-            <li>
-              {isSidebarOpen ? (
-                <IoClose
-                  className="hamburger-icon cross"
-                  onClick={toggleMenu}
-                />
-              ) : (
-                <IoReorderThreeOutline
-                  className="hamburger-icon"
-                  onClick={toggleMenu}
-                />
-              )}
-            </li>
-          </ul>
+          <div className="hamburger-icon" onClick={toggleMenu}>
+            {isSidebarOpen ? (
+              <IoClose className="cross" />
+            ) : (
+              <IoReorderThreeOutline />
+            )}
+          </div>
         </div>
       </div>
     </div>
